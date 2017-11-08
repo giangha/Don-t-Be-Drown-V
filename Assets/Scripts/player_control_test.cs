855a74710add696105a690f8e18a00894de957c7
giangha <gi4ngh495@gmail.com>
2017-11-08 01:51:04 -0600

    add person in

--- Assets/Scripts/player_control_test.cs
+++ Assets/Scripts/player_control_test.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class player_control_test : MonoBehaviour {
     public Rigidbody2D rb;
     public float speed;
-
+    public bool scare;
+    private Animator amin;
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody2D>();
+        amin = gameObject.GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        amin.SetFloat("speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        amin.SetBool("scare", scare);
         float moveHorizontal = Input.GetAxis("Horizontal");
 		if(Input.GetKey("right")) {
 			transform.localScale = new Vector3(-1,1,1);
@@ -24,5 +27,19 @@ public class player_control_test : MonoBehaviour {
 
         Vector2 move = new Vector2(moveHorizontal, 0.0f);
         rb.velocity = move * speed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Alligator"))
+        {
+            scare = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Alligator"))
+        {
+            scare = true;
+        }
     }
 }
